719ce3bf27b76718bcc4d5d136443c11338ebb0b
Soocyang <36953143+Soocyang@users.noreply.github.com>
2022-10-03 23:22:26 +0800

    refactor error handler

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@ import express from 'express'
 import { json } from 'body-parser'
 
 import routes from './routes'
-import { logErrors } from './middleware'
-import { errorHandler, Exception } from './config'
+import { errorHandler, logErrors } from './middleware'
+import { Exception } from './config'
 
 (() => {
 
@@ -14,9 +14,9 @@ import { errorHandler, Exception } from './config'
     next(new Exception('E0006', `Can't find ${req.originalUrl} on this service`, 404))
   })
 
+  app.use(logErrors)
   app.use(errorHandler)
 
-  app.use(logErrors)
   app.listen(3000)
   console.info('Express app started on port 3000')
 })()
--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,1 +1,2 @@
 export * from './errorLog'
+export * from './errorHandler'
