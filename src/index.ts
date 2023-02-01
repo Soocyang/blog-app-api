ac413eb35b1497c0eed61e65dff766614fd1d742
Soocyang <36953143+Soocyang@users.noreply.github.com>
2023-02-01 22:53:59 +0800

    add swagger ui

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import swaggerUi from 'swagger-ui-express'
+import swaggerJSDoc from 'swagger-jsdoc';
 import express from 'express'
 import { json } from 'body-parser'
 
@@ -6,10 +8,30 @@ import { errorHandler, logErrors } from './middleware'
 import { Exception } from './config'
 
 (() => {
-
+  const APP_PORT = process.env.PORT || 4000
   const app = express()
   app.use(json())
   app.use('/', routes)
+
+  const options = {
+    definition: {
+      openapi: '3.0.0',
+      info: {
+        title: 'Blog API',
+        description: 'This is a REST API application made with Express. It retrieves blog posts related data', 
+        version: '1.0.0',
+      },
+      servers: [
+        {
+          url: 'http://localhost:4000',
+          description: 'Development server',
+        },
+      ],
+    },
+    apis: ['./src/routes/**.ts'],
+  }
+  const swaggerSpec = swaggerJSDoc(options);
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec))
   app.all('*', (req, res, next) => {
     next(new Exception('E0006', `Can't find ${req.originalUrl} on this service`, 404))
   })
@@ -17,8 +39,8 @@ import { Exception } from './config'
   app.use(logErrors)
   app.use(errorHandler)
 
-  app.listen(process.env.PORT)
-  console.info(`Blog api server started on port ${process.env.PORT}`)
+  app.listen(APP_PORT)
+  console.info(`Blog api server started on port ${APP_PORT}`)
 })()
 
 
--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -5,10 +5,92 @@ import { getPosts, createPost, deletePost, getPostById, updatePostById } from '.
 
 const router = Router()
 
+/**
+ * @swagger
+ * /posts:
+ *   get:
+ *     description: Retrieve a list of posts
+ *     responses:
+ *       200:
+ *         description: Returns a list of posts
+ */
 router.get('/', getPosts);
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   get:
+ *     description: Retrieve a post details by id
+ *     parameters:
+ *           - in: path
+ *             name: id
+ *             required: true
+ *             description: Numeric ID of the post to retrieve.
+ *             schema:
+ *               type: string
+ *     responses:
+ *       200:
+ *         description: Returns a post details
+ */
 router.get('/:id', getPostById);
+
+/**
+ * @swagger
+ * /posts:
+ *   post:
+ *     description: Create a post 
+ *     requestBody:
+ *           required: true
+ *           content:
+ *             application/json:
+ *               schema:
+ *                 type: object
+ *                 properties:
+ *                   title: 
+ *                      type: string
+ *                      example: How to Typescript
+ *                   meta_title: 
+ *                      type: string
+ *                      example: Typescript
+ *                   content: 
+ *                      type: string
+ *                      example: Lorem ipsum x1000
+ *                   summary: 
+ *                      type: string
+ *                      example: Lorem ipsum
+ *                   url_key: 
+ *                      type: string
+ *                      example: how_to_typescript
+ *                   is_published: 
+ *                      type: boolean
+ *                      example: false
+ *     responses:
+ *       200:
+ *         description: Returns the created post
+ */
 router.post('/', createPost);
-router.delete('/:id', deletePost);
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   put:
+ *     description: Update a post by id
+ *     responses:
+ *       200:
+ *         description: Returns the updated post details
+ */
 router.put('/:id', updatePostById);
+
+/**
+ * @swagger
+ * /posts/{id}:
+ *   delete:
+ *     description: Delete a post by id
+ *     responses:
+ *       200:
+ *         description: Returns the deleted process info
+ */
+router.delete('/:id', deletePost);
+
 
 export default router
